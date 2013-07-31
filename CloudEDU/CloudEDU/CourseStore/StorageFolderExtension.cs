6d493ff6ee510ba3859b7a7e24de941a49dc5ef3
Max Lu <luyirenmax@gmail.com>
2013-08-01 04:59:55 +0800

    test file exits , download file, launch file with other program

---
+++ CloudEDU/CloudEDU/CourseStore/StorageFolderExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Search;
+
+namespace CloudEDU.CourseStore
+{
+    public static class StorageFolderExtension
+    {
+        public static async Task<bool> CheckFileExisted(this StorageFolder folder, string fileName)
+        {
+            return folder != null &&
+                   (await (
+                              folder.CreateFileQueryWithOptions(
+                                  new QueryOptions
+                                  {
+                                      FolderDepth = FolderDepth.Shallow,
+                                      UserSearchFilter = "System.FileName:\"" + fileName + "\""
+                                  })).GetFilesAsync()).Count > 0
+                       ? true
+                       : false;
+        }
+    }
+}
